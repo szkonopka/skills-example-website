96d51795b3dd382f426b17341bfd142667c11130
Szymon Konopka <samzonee@gmail.com>
2018-07-17 22:04:21 +0200

    Grid, sections - prices, offer, additional info, footer.

--- src/animations.js
+++ src/animations.js
@@ -2,12 +2,23 @@ export function toggleMenu()
 {
 	const menu = getMenuDOMElem();
 	const headerHamburger = getHeaderHamburgerDOMElem();
+	const logoSrcLight = './img/vr-logo-w.svg';
+	const logoSrcDark = './img/vr-logo.svg';
+	const logoContainer = document.querySelector('#logo img');
+	const currentLogoSrc = logoContainer.getAttribute('src');
 
 	if(menu === null || headerHamburger === null)
 		return false;
 
 	menu.classList.toggle('main-menu_active');
 	headerHamburger.classList.toggle('header__hamburger_active');
+
+	if(currentLogoSrc === logoSrcLight)
+		setTimeout(() => {
+			logoContainer.setAttribute('src', logoSrcDark);
+		}, 200);
+	else
+		logoContainer.setAttribute('src', logoSrcLight);
 
 	return true;
 }
